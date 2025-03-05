3e08f80e570da150eb988876b40307bdddb65dd7
marta <ess.marta.oleszkiewicz@gmail.com>
2025-03-05 16:58:36 +0100

    opponents pododawane puste klasy

--- src/Opponent1.java
+++ src/Opponent1.java
@@ -1,6 +1,8 @@
 import javafx.scene.image.Image;
 // Pojazd pierwszy porusza się z maksymalną prędkością po liniach prostych i mocno hamuje przed każdym zakrętem
 
+//sprawdza w pętli pod spodem ile jest pixeli do sciany --> <200 zwalnia a jak jest 50 od sciany to skreca
+//sprawdzamy w którą stronę się obracamy
 public class Opponent1 extends Opponents implements OpponentsAI{
     public Opponent1(double x, double y, Image imageView) {
         super(x, y, imageView);
