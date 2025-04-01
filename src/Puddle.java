987439e6e2998ab8cddc9165a84936c41995313d
Daria <cortisol.the.stres5er@gmail.com>
2025-04-01 17:52:05 +0200

    Heyo, i fixed the puddles so they stay in bounds
    Cheers<3
    Daria
    
    17:51
    01.04.2025

--- src/Puddle.java
+++ src/Puddle.java
@@ -3,7 +3,9 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.util.Duration;
 
 import java.util.ArrayList;
@@ -19,10 +21,13 @@ public class Puddle extends ImageView implements Obstacle{
     public static AnchorPane root;
     public int timer;
 
+    private Image maska = new Image("file:maska.png");
+
     public static void puddleFactory(AnchorPane anchorPane){
         root = anchorPane;
         Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
             Puddle puddle = new Puddle(Math.random() * Main.WIDTH, Math.random() * (Main.HEIGHT - 200), Math.random() < 0.5 );
+            puddle.getPuddlesInRightRange();
             puddleList.add(puddle);
             root.getChildren().add(puddle);
 
@@ -66,6 +71,38 @@ public class Puddle extends ImageView implements Obstacle{
             timer = random;
         }
 
+    }
+
+    public void getPuddlesInRightRange(){
+        double NewX = Math.random()*Main.WIDTH;
+        double NewY = Math.random()*Main.HEIGHT;
+        boolean foundNewPlace = false;
+        while (foundNewPlace == false){
+
+            PixelReader maskaReader = maska.getPixelReader();
+            if (maskaReader == null) {
+
+                return;
+            }
+
+
+            Color maskaColor = maskaReader.getColor((int) (NewX - this.getFitWidth()), (int) (NewY - this.getFitHeight()));
+//            System.out.println("("+ NewX + NewY+ ")"+ maskaColor);
+            if (maskaColor.equals(Color.rgb(0, 0, 0, 1))){
+                NewX = Math.random()*Main.WIDTH;
+                NewY = Math.random()*Main.HEIGHT;
+            }
+            else{
+                foundNewPlace = true;
+                TokyoDriftTheme.level1Root.getChildren().remove(this);
+                setLayoutX(NewX);
+                setLayoutY(NewY);
+                TokyoDriftTheme.level1Root.getChildren().add(this);
+
+
+
+            }
+        }
     }
 
 
