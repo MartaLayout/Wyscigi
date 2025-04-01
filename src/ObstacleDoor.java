674d64eecba485af21c6bff400f8801c20725ad7
marta <ess.marta.oleszkiewicz@gmail.com>
2025-04-01 12:21:36 +0200

    Marta: working (or not really?) on obstacle doors [12:21]

--- src/ObstacleDoor.java
+++ src/ObstacleDoor.java
@@ -72,10 +72,10 @@ public class ObstacleDoor {
     public void collision(Player player){ //TODO this plis (im crying)
         if (this.imageView.getBoundsInParent().intersects(player.getBoundsInParent())){
             System.out.println("collision with doors");
-            Car.moveForward = false;
+//            Car.moveForward = false;
             if (currentImage <=3 || currentImage>= 14){
                 System.out.println("pass");
-                Car.moveForward = true;
+//                Car.moveForward = true;
             }
         }
     }
